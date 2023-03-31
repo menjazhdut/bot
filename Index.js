72ff03b2f3ff90eac6f8350659d705752b4202f3
menjazhdut <129472273+menjazhdut@users.noreply.github.com>
2023-03-31 18:10:05 +0000

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,46 +1,22 @@
+Node.js
 const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
-const fs = require('fs');
+const axios = require('axios');
 
-// Задаем токен нашего бота
-const token = '6163045567:AAEJZnGXbNuIq-LiE8ddFAbzLIb54Q6q7Bs';
+const token = '6008668981:AAE-GMnCqmITBohOVJ2o3AN8hzYjvPqnjTA'; // здесь нужно указать токен вашего бота
+const bot = new TelegramBot(token, {polling: true});
 
-// Создаем экземпляр бота
-const bot = new TelegramBot(token, { polling: true });
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
 
-// Ответ на сообщение старта
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Привет! Выберите, какой пол вам интересен: /boy или /girl.');
-});
-
-// Обработчик команды /boy
-bot.onText(/\/boy/, (msg) => {
-  // Получаем случайное изображение мужчины с помощью API Unsplash
-  request.get('https://api.unsplash.com/photos/random?query=man&client_id=YOUR_ACCESS_KEY', (err, res, body) => {
-    if (err) {
-      bot.sendMessage(msg.chat.id, 'Произошла ошибка. Попробуйте еще раз.');
-    } else {
-      const imageUrl = JSON.parse(body).urls.regular;
-      // Скачиваем изображение по URL
-      request.get(imageUrl).pipe(fs.createWriteStream('image.jpg')).on('close', () => {
-        bot.sendPhoto(msg.chat.id, fs.readFileSync('image.jpg'));
-      });
+  if (messageText === '/quote') {
+    try {
+      const response = await axios.get('https://api.kanye.rest/');
+      const quote = response.data.quote;
+      bot.sendMessage(chatId, quote);
+    } catch (error) {
+      console.log(error);
+      bot.sendMessage(chatId, 'Произошла ошибка при получении цитаты :(');
     }
-  });
-});
-
-// Обработчик команды /girl
-bot.onText(/\/girl/, (msg) => {
-  // Получаем случайное изображение женщины с помощью API Unsplash
-  request.get('https://api.unsplash.com/photos/random?query=woman&client_id=YOUR_ACCESS_KEY', (err, res, body) => {
-    if (err) {
-      bot.sendMessage(msg.chat.id, 'Произошла ошибка. Попробуйте еще раз.');
-    } else {
-      const imageUrl = JSON.parse(body).urls.regular;
-      // Скачиваем изображение по URL
-      request.get(imageUrl).pipe(fs.createWriteStream('image.jpg')).on('close', () => {
-        bot.sendPhoto(msg.chat.id, fs.readFileSync('image.jpg'));
-      });
-    }
-  });
+  }
 });
